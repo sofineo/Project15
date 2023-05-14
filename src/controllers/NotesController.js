b4b8afb26749ee27f5bca076313fac13b5de6df2
Sofia Neo <sofineo@gmail.com>
2023-05-13 21:01:37 -0300

    atualização

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -87,7 +87,7 @@ class NotesController {
 
     const userTags = await knex("tags").where({ user_id })
     const notesWithTags = notes.map(note => {
-      const noteTags = userTags.filter(tag => tag.note_id === notes.id)
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
 
       return {
         ...note,
